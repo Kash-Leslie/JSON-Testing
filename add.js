3d9baec3728d28bfa2809d0157b94e40239433b3
Kash-Leslie <71522973+Kash-Leslie@users.noreply.github.com>
2021-02-02 12:39:29 -0600

    Update add.js

--- add.js
+++ add.js
@@ -1,4 +1,5 @@
 function click(){
+  document.preventDefault();
   const form = document.getElementByClassName('test')
   string json = JsonConvert.SerializeObject(form, Formatting.Indented);
   document.write(json);
