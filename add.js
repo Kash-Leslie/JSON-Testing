dca8b558f77936baecbb97032b5adec97c5f2b13
Kash-Leslie <71522973+Kash-Leslie@users.noreply.github.com>
2021-02-03 12:44:41 -0600

    Update add.js

--- add.js
+++ add.js
@@ -1,6 +1,3 @@
 function click(){
-  document.preventDefault();
-  const form = document.getElementByClassName('test')
-  string json = JsonConvert.SerializeObject(form, Formatting.Indented);
-  document.write(json);
+  window.alert("hello")
 }
